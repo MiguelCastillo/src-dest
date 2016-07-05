acd4ff09f4a526fc47197188b38fa3b38bed181e
Miguel Castillo <manchagnu@gmail.com>
2016-07-05 08:46:30 -0400

    Adding initial implementation with unit tests

---
+++ test/spec/file.js
@@ -0,0 +1,97 @@
+import { expect } from "chai";
+import File from "../../index";
+
+describe("File unit test", function() {
+  var act, result, options;
+
+  beforeEach(function() {
+    act = () => result = new File(options);
+  });
+
+  describe("When creating a File with no options", function() {
+    beforeEach(function() {
+      options = null;
+      act();
+    });
+
+    it("then the file has no destination", function() {
+      expect(result.dest).to.be.null;
+    });
+
+    it("then the file has an empty src", function() {
+      expect(result.src).to.be.empty;
+    });
+
+    it("then the current directory contains src-dest", function() {
+      expect(result.cwd).to.contain("/src-dest");
+    });
+  });
+
+  describe("When creating a File with only a string as the input", function() {
+    beforeEach(function() {
+      options = "test/spec/file.js";
+      act();
+    });
+
+    it("then the file has one item in source", function() {
+      expect(result.src).to.have.lengthOf(1);
+    });
+
+    it("then the source item is a path that contains the input", function() {
+      expect(result.src[0]).to.contain("src-dest/test/spec/file.js");
+    });
+
+    it("then the current directory contains src-dest", function() {
+      expect(result.cwd).to.contain("/src-dest");
+    });
+  });
+
+  describe("When creating a File with two strings as the input", function() {
+    beforeEach(function() {
+      options = ["./index.js", "test/spec/file.js"];
+      act();
+    });
+
+    it("then the file has two items in source", function() {
+      expect(result.src).to.have.lengthOf(2);
+    });
+
+    it("then the first source item is a path that contains the input", function() {
+      expect(result.src[0]).to.contain("src-dest/index.js");
+    });
+
+    it("then the second source item is a path that contains the input", function() {
+      expect(result.src[1]).to.contain("src-dest/test/spec/file.js");
+    });
+
+    it("then the current directory contains src-dest", function() {
+      expect(result.cwd).to.contain("/src-dest");
+    });
+  });
+
+  describe("When creating a File with two src strings in a configuration object", function() {
+    beforeEach(function() {
+      options = {
+        src: ["./index.js", "test/spec/file.js"]
+      };
+
+      act();
+    });
+
+    it("then the file has two items in source", function() {
+      expect(result.src).to.have.lengthOf(2);
+    });
+
+    it("then the first source item is a path that contains the input", function() {
+      expect(result.src[0]).to.contain("src-dest/index.js");
+    });
+
+    it("then the second source item is a path that contains the input", function() {
+      expect(result.src[1]).to.contain("src-dest/test/spec/file.js");
+    });
+
+    it("then the current directory contains src-dest", function() {
+      expect(result.cwd).to.contain("/src-dest");
+    });
+  });
+});
